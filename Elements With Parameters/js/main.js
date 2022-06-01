c269cb79af16d3bbe66cb71d0688d27202f030aa
PuPunPa <josepablolppl@gmail.com>
2022-05-31 21:28:27 -0500

    Exercise 2 + Extra

--- Elements With Parameters/js/main.js
+++ Elements With Parameters/js/main.js
@@ -1,8 +1,9 @@
 var data = [25, 20, 15, 10, 5];
+var height = 400
 
 var svg = d3.select("#chart-area").append("svg")
 	.attr("width", 400)
-	.attr("height", 400);
+	.attr("height", height);
     
 var rects = svg.selectAll("rect")
         .data(data)
@@ -11,7 +12,7 @@ var rects = svg.selectAll("rect")
 rects.enter()
     .append("rect")
 	.attr("x", (d) =>{return d;})
-	.attr("y", 20)
+	.attr("y", height-40)
 	.attr("width", 40)
 	.attr("height", (d) =>{return d;})
 	.attr("fill","red");
